fbd1d0494b1af7878a782aebae0cb66e1e0e549e
Juan <jberrioscifuentes@gmail.com>
2019-10-10 18:08:15 -0300

    C

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
     <title>NFIT</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:700|Roboto:400,700,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/app.css?ver=1.1">
   </head>
   <body class="font-principal bg-nfit-body text-white w-full overflow-x-hidden">
     <main class="w-full">
