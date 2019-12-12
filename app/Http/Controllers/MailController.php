244224b6d0508439a776aa975ce72157d6dac831
boryeira <jmanuel.jorquera@gmail.com>
2019-12-12 11:17:11 -0300

    redirect

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -6,12 +6,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 use App\Mail\ContactForm;
+use Redirect;
 
 class MailController extends Controller
 {
     public function contactForm(Request $request)
     {
         Mail::to('contacto@nfit.app')->send(new ContactForm($request));
+        return Redirect::back();
 
     }
 }
