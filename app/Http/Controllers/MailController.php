55a0abda86ccd08517167d0cd0339ea5cc6fe421
boryeira <jmanuel.jorquera@gmail.com>
2019-12-12 11:51:02 -0300

    email

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -7,13 +7,14 @@ use Illuminate\Support\Facades\Mail;
 
 use App\Mail\ContactForm;
 use Redirect;
+use Session;
 
 class MailController extends Controller
 {
     public function contactForm(Request $request)
     {
-        Mail::to('jmanuel.jorquera@gmail.com')->send(new ContactForm($request));
+        Mail::to('pablo.rioseco@asomic.com')->send(new ContactForm($request));
+        Session::flash('successs','Formulario enviado con exito!');
         return Redirect::back();
-
     }
 }
