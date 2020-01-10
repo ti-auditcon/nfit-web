1fe29ec9269929965e601ba13da764c23cf05512
Juan <jberrioscifuentes@gmail.com>
2020-01-10 12:10:25 -0300

    tag manager code

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -6,8 +6,21 @@
     <title>NFIT</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:700|Roboto:400,700,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/app.css?ver=1.1">
+    <!-- Google Tag Manager -->
+    <script>
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5583ZSF');
+    </script>
+    <!-- End Google Tag Manager -->
   </head>
   <body class="font-principal bg-nfit-body text-white w-full overflow-x-hidden">
+    <!-- Google Tag Manager (noscript) -->
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5583ZSF"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <main class="w-full">
 
       {{-- Header --}}
